11bc1748d255a23b3af9cb6826ee3a715fcf435e
fana121 <79257110+fana121@users.noreply.github.com>
2021-03-08 22:44:48 +0700

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -15,7 +15,7 @@ let handler  = async (m, { conn, args }) => {
     })
   }
 }
-handler.help = ['stiker (caption|reply media)', 'stiker <url>']
+handler.help = ['stiker (reply media)', 'stiker <url>']
 handler.tags = ['sticker']
 handler.command = /^stic?ker$/i
 handler.owner = false
