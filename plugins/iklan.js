85ac228be45735be0087feb0fdd9730e1322c32f
fana121 <79257110+fana121@users.noreply.github.com>
2021-03-12 11:13:07 +0700

    Fana gamntenk

--- plugins/iklan.js
+++ plugins/iklan.js
@@ -1,1 +1,31 @@
-const iklan1 = (pushname, prefix, botName, ownerName) => { 	return `🔰 -----[ *「 IKLAN ${botName} 」* ]----- 🔰──────────────────────────────◯ *DAFTAR SEWA & BUAT BOT :*◯ *SEWA : 25K/GRUP (BULAN)*◯ *BUAT : 100K (BISA JADI OWNER)*◯ *PEMBAYARAN BISA MELALUI :*◯ *OVO, GOPAY, DANA, PULSA+10K*──────────────────────────────◯ *KEUNTUNGAN SEWA BOT :*◯ *1. BISA MEMASUKAN BOT KE GROUP*◯ *2. BISA MENGGUNAKAN FITUR PREMIUM*◯ *KEUNTUNGAN BUAT BOT :*◯ *1. BISA MENJADI OWNER BOT SENDIRI*◯ *2. BISA MENGGANTI NAMA BOT SENDIRI*◯ *3. BISA MEMBAWA BOT KE GROUP*◯ *4. BISA MENGGUNAKAN COMMAND OWNER*◯ *5. BISA MENYEWAKAN BOT KEMBALI*──────────────────────────────◯ *JIKA MINAT IKLAN DIATAS*◯ *HARAP HUBUNGI NOMOR DIBAWAH :*◯ *wa.me/6285559240360*──────────────────────────────🔰 -----[ *「 POWERED BY ${ownerName} 」* ]----- 🔰`} exports.iklan1 = iklan1
+const iklan1 = (pushname, prefix, botName, ownerName) => { 
+	return `🔰 -----[ *「 IKLAN ${botName} 」* ]----- 🔰
+──────────────────────────────
+◯ *DAFTAR SEWA & BUAT BOT :*
+◯ *SEWA : 15K/GRUP (BULAN)*
+◯ *BUAT : 50K (BISA JADI OWNER)*
+◯ *PEMBAYARAN BISA MELALUI :*
+◯ *GOPAY, TELKOMSEL(+10K), INDOSAT(+5K)*
+──────────────────────────────
+◯ *KEUNTUNGAN SEWA BOT :*
+◯ *1. BISA MEMASUKAN BOT KE GROUP*
+◯ *2. BISA MENGGUNAKAN FITUR PREMIUM*
+◯ *NB: 1. GUNAKAN BOT SECUKUPNYA, JANGAN SPAM KARENA BOT BISA KENON OLEH PIHAK WHATSAPP*
+◯     *2. DILARANG KERAS CHAT PRIB BOT*
+◯ *KEUNTUNGAN BUAT BOT :*
+◯ *1. BISA MENJADI OWNER BOT SENDIRI*
+◯ *2. BISA MENGGANTI NAMA BOT SENDIRI*
+◯ *3. BISA MEMBAWA BOT KE GROUP*
+◯ *4. BISA MENGGUNAKAN COMMAND OWNER*
+◯ *5. BISA MENYEWAKAN BOT KEMBALI*
+◯ *NB: DIKASIH TUTOR BUAT BOT, WORK? YA PASTI (PAHAMI DULU BARU PRAKTEKIN)
+──────────────────────────────
+◯ *JIKA MINAT IKLAN DIATAS*
+◯ *HARAP HUBUNGI NOMOR DIBAWAH :*
+◯ *wa.me/6285891927691 atau ketik #owner*
+──────────────────────────────
+🔰 -----[ *「 POWERED BY ${ownerName} 」* ]----- 🔰
+`
+}
+
+exports.iklan1 = iklan1
