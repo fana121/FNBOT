f5aebdc323715a5ecac1f1ff1bd8e88f5fdf6c34
Fantod <79257110+fana121@users.noreply.github.com>
2021-03-14 00:17:18 +0700

    Create katapatrick.js

---
+++ plugins/katapatrick.js
@@ -0,0 +1,34 @@
+let handler  = async (m, { conn }) => {
+  conn.reply(m.chat,`“${pickRandom(global.katapatrick)}”`, m)
+}
+handler.help = ['katapatrick']
+handler.tags = ['quotes']
+handler.command = /^(katapatrick)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+// jangan diubah
+global.katapatrick =
+  [
+  "Tenang saja Rocky, kamu bisa pergi saat kamu mau",
+  "Mungkin aku bodoh, tapi aku tidak dungu",
+  "Pengetahuan tidak bisa mengganti persahabatan. Aku lebih baik menjadi seorang idiot!",
+  "Hidup ini memang tidak adil, maka biasakanlah dirimu!",
+  "Kadang kita harus melihat jauh ke dalam diri kita untuk menyelesaikan masalah",
+  "Sekarang yang kuperlukan adalah sebuah kumis ajaib dan semua mimpiku akan terwujud!",
+  "Aku tidak berkomentar tentang caramu menjalani hidup",
+  ]
