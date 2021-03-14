b9246b60c0624d498b79b10fb275cdd9be95786d
Fantod <79257110+fana121@users.noreply.github.com>
2021-03-14 20:26:21 +0700

    Update pengumuman.js

--- plugins/pengumuman.js
+++ plugins/pengumuman.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, participants }) => {
   conn.reply(m.chat, text, m, { contextInfo: { mentionedJid: users } })
 }
 handler.help = ['pengumuman','announce','hidetag'].map(v => v + ' [teks]')
-handler.tags = ['tools']
+handler.tags = ['admin']
 handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
 handler.owner = false
 handler.mods = false
