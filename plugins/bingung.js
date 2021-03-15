bc4d3f43193824f09ec25757e7e5c28fffedbc6a
Fantod <79257110+fana121@users.noreply.github.com>
2021-03-15 12:04:42 +0700

    Create bingung.js

---
+++ plugins/bingung.js
@@ -0,0 +1,44 @@
+🔰 -----[ *PANDUAN PENGGUNAAN FNBOT* ]----- 🔰
+Hallo, %name 👋
+Berikut adalah cara penggunaan *FNBOT*
+         ────────────────
+Kalo gak paham tuh baca dulu -_-
+         ────────────────
+➸ *${prefix}sticker*
+Tag salah satu gambar & ketik ${prefix}sticker
+➸ *${prefix}ttp*
+Text to sticker contoh : ${prefix}ttp Fantod
+➸ *${prefix}tts*
+Suara google contoh : ${prefix}tts id Hallo Ramlan
+➸ *${prefix}toimg*
+Reply sticker yang mau dijadiin image
+➸ *${prefix}nulis*
+Untuk menulis dibuku
+➸ *${prefix}nulisfolio*
+Untuk menulis di folio
+➸ *${prefix}quotes*
+Random quotes
+➸ *${prefix}hidetag*
+Nge tag semua member grup
+➸ *${prefix}ytmp3*
+Mendownload lagu dari youtube Gunakan Link Ya Kak
+➸ *${prefix}ytmp4*
+Mendownload video dari youtube, gunakan link ya kak
+➸ *${prefix}profile*
+Mengambil profil member, contoh : ${prefix}profile @user
+➸ *${prefix}tahta*
+Membuat logo, contoh : ${prefix}tahta Fana
+➸ *${prefix}jadibot*
+Mengirim qr untuk menjadi bot sementara
+➸ *${prefix}iklan*
+Untuk menampilkan daftar sewa bot dan buat bot
+➸ *${prefix}readmore*
+Membuat text baca selengkapnya, contoh : ${prefix}readmore text|text
+         ────────────────
+Note : Sisanya gunain otak kalian aja cape w jelasin :)
+         ────────────────
+🔰 -----[ *TUTOR BY FANA* ]----- 🔰
+`
+}
+
+exports.cara = cara
