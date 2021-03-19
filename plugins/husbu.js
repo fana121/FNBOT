dc2101949ddb9ee0c1ed406fbe543f30dae493ef
Fantod <79257110+fana121@users.noreply.github.com>
2021-03-19 18:01:19 +0700

    Update husbu.js

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-  conn.reply(m.chat,`${pickRandom(global.husbu)}`, m)
+  conn.sendFile(m.chat,`${pickRandom(global.husbu)}`, m)
 }
 handler.help = ['husbu']
 handler.tags = ['anime']
