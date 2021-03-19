b58b6794947860c5b077bebae3a66861267d8616
Fantod <79257110+fana121@users.noreply.github.com>
2021-03-19 18:00:41 +0700

    Update loli.js

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,5 +1,5 @@
 let handler  = async (m, { conn }) => {
-  conn.reply(m.chat,`${pickRandom(global.loli)}`, m)
+  conn.sendFile(m.chat,`${pickRandom(global.loli)}`, m)
 }
 handler.help = ['loli']
 handler.tags = ['anime']
