51d6274e4fd53e3443138a776784f2a663248c08
Fantod <79257110+fana121@users.noreply.github.com>
2021-03-19 18:48:36 +0700

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -24,15 +24,17 @@ function pickRandom(list) {
 // jangan diubah
 global.waifu =
   [
-"Mea Kurosaki from To Love-Ru,\nhttp://randomwaifu.altervista.org/images/0008.png",
-"Ochaco Uraraka from My Hero Academia,\nhttp://randomwaifu.altervista.org/images/0057.png",
-"Jibril from No Game No Life,\nhttp://randomwaifu.altervista.org/images/0056.png",
-"Tohru from Miss Kobayashi's Dragon Maid,\nhttp://randomwaifu.altervista.org/images/0045.png",
-"Android N21 from Dragon Ball,\nhttp://randomwaifu.altervista.org/images/0078.png",
-"Misaki Okusawa from BanG Dream!,\nhttp://randomwaifu.altervista.org/images/0035.png",
-"Reina Kousaka from Hibike! Euphonium,\nhttp://randomwaifu.altervista.org/images/0027.png",
-"Tohru from Miss Kobayashi's Dragon Maid,\nhttp://randomwaifu.altervista.org/images/0045.png",
-"Azusa Azuki from Hentai oji to warawanai neko,\nhttp://randomwaifu.altervista.org/images/0086.png",
-"Raphtalia from The Rising of the Shield Hero,\nhttp://randomwaifu.altervista.org/images/0085.png",
-"Uzaki Hana from Uzaki-chan wa Asobitai,\nhttp://randomwaifu.altervista.org/images/0013.png",
+"https://user-images.githubusercontent.com/79257110/111774965-bb391980-88e2-11eb-91ea-48c23712d03e.jpg",
+"https://user-images.githubusercontent.com/79257110/111774971-bd9b7380-88e2-11eb-80aa-0cb1a3061f69.jpg",
+"https://user-images.githubusercontent.com/79257110/111774974-becca080-88e2-11eb-9491-e26eeacdb2cb.jpg",
+"https://user-images.githubusercontent.com/79257110/111774979-bffdcd80-88e2-11eb-983d-a1f944c26ae9.jpg",
+"https://user-images.githubusercontent.com/79257110/111774985-c12efa80-88e2-11eb-8872-6a06eb0cc1a6.jpg",
+"https://user-images.githubusercontent.com/79257110/111774989-c2602780-88e2-11eb-8bb3-0a9d57e072db.jpg",
+"https://user-images.githubusercontent.com/79257110/111774991-c2f8be00-88e2-11eb-98a7-cba8c4a888c6.jpg",
+"https://user-images.githubusercontent.com/79257110/111774992-c3915480-88e2-11eb-9078-109cdadb4135.jpg",
+"https://user-images.githubusercontent.com/79257110/111775000-c55b1800-88e2-11eb-9177-67753851939f.jpg",
+"https://user-images.githubusercontent.com/79257110/111775004-c5f3ae80-88e2-11eb-9e69-0773d13407d4.jpg",
+"https://user-images.githubusercontent.com/79257110/111775008-c724db80-88e2-11eb-8d9b-bb479648f38e.jpg",
+"https://user-images.githubusercontent.com/79257110/111775012-c8560880-88e2-11eb-8256-6924d8746989.jpg",
+"https://user-images.githubusercontent.com/79257110/111775013-c9873580-88e2-11eb-8296-669ec1525d2b.jpg",
 ]
