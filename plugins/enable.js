d49acdb3816b5bd74c652a1638d98c415f75b13b
Fantod <79257110+fana121@users.noreply.github.com>
2021-03-24 00:02:24 +0700

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -35,7 +35,7 @@ ${usedPrefix}disable welcome
 `.trim())
 }
 handler.help = ['en', 'dis'].map(v => v + 'able <option>')
-handler.tags = ['group', 'owner']
+handler.tags = ['owner']
 handler.command = /^((en|dis)able|(tru|fals)e|(turn)?o(n|ff))$/i
 
 module.exports = handler
