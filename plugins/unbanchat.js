228807e029917fd7f3babfcaf8fde4cccad93295
Fantod <79257110+fana121@users.noreply.github.com>
2021-03-24 20:53:30 +0700

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -2,9 +2,9 @@ let handler = async (m, { conn }) => {
   global.DATABASE._data.chats[m.chat].isBanned = false
   m.reply('*SUCCESS UNBANNED*')
 }
-handler.help = ['unban']
+handler.help = ['unbanchat']
 handler.tags = ['owner']
-handler.command = /^unban$/i
+handler.command = /^unbanchat$/i
 handler.owner = true
 
 module.exports = handler
