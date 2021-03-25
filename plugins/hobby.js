97a342f1684a788a8a82f50304ac8a8c26b5b57f
Fantod <79257110+fana121@users.noreply.github.com>
2021-03-25 14:11:56 +0700

    Create hobby.js

---
+++ plugins/hobby.js
@@ -0,0 +1,30 @@
+let handler = async (m, { conn, command, text }) => {
+  conn.reply(m.chat, `
+*Pertanyaan:* ${command} ${text}
+*Jawaban:* ${pickRandom(['Memasak','Membantu Atok','Mabar','Nobar','Sosmed an','Membantu Orang lain','Nonton Anime','Nonton Drakor','Naik Motor','Nyanyi','Menari','Bertumbuk','Menggambar','Foto fotoan Ga jelas','Maen Game','Berbicara Sendiri','Ngentot','Coli','Colmek','Kentu','Nonton bokep','Jilat memek'])}
+`.trim(), m, m.mentionedJid ? {
+    contextInfo: {
+      mentionedJid: m.mentionedJid
+    }
+  } : {})
+}
+handler.help = ['hobby <@user>']
+handler.tags = ['kerang']
+handler.command = /^(hobby)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(Math.random() * list.length)]
+}
+
