bbd36fbaf2b47f788d77e62c3a6020082a469866
Fantod <79257110+fana121@users.noreply.github.com>
2021-10-16 21:56:01 +0700

    Update

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6285891927691', 'Fantodd', m)
+  // this.sendContact(m.chat, '62895712546050', 'SHP', m)
+  this.sendContact(m.chat, '62895712546050', 'SHP', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
